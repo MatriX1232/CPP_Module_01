5b9e3e81585171fe880765f74478afa9c3ebe4e4
msolinsk <msolinsk@student.42warsaw.pl>
2024-11-02 18:26:49 +0100

    Finished ex05

---
+++ ex05/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msolinsk <msolinsk@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/02 18:06:53 by msolinsk          #+#    #+#             */
+/*   Updated: 2024/11/02 18:17:17 by msolinsk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int main()
+{
+	Harl harl;
+
+	harl.complain("debug");
+	harl.complain("info");
+	harl.complain("warning");
+	harl.complain("error");
+
+	return (0);
+}
